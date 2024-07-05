b4d82452f77e472e9e985c3110bf34d8c6ffea46
cptncuddles <159074823+cptncuddles@users.noreply.github.com>
2024-07-05 14:47:29 -0500

    Update StarDew_Cooking.py

--- StarDew_Cooking.py
+++ StarDew_Cooking.py
@@ -9,7 +9,7 @@ def to_int(data_frame, columns):
         data_frame[ele] = data_frame[ele].astype(int)
 
 
-spring = pd.read_csv('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Spring Vegtables.csv')
+spring = pd.read_csv('file_path\\Spring Vegtables.csv')
 pd.set_option('display.max_columns', None)
 
 spring.replace('Null', '0', inplace=True, regex=True)
@@ -21,9 +21,9 @@ columns = ['Dehydrator Artisan', 'Dehydrator', 'Preserve Jar', 'Keg Artisan', 'K
 to_float(spring, columns)
 
 #print(spring.dtypes)
-spring.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Spring Vegetable.parquet',engine='fastparquet')
+spring.to_parquet('file_path\\Spring Vegetable.parquet',engine='fastparquet')
 
-summer = pd.read_csv('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Summer Vegtables.csv')
+summer = pd.read_csv('file_path\\Summer Vegtables.csv')
 summer.replace('Null', '0', inplace=True, regex= True)
 summer.rename(columns={'Catagory' : 'Category'}, inplace=True)
 
@@ -31,30 +31,30 @@ to_float(summer, columns)
 
 summer['Preserve Jar Artisan'] = summer['Preserve Jar Artisan'].astype(float)
 
-summer.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Summer Vegetables.parquet',engine='fastparquet')
+summer.to_parquet('file_path\\Summer Vegetables.parquet',engine='fastparquet')
 
-fall = pd.read_csv('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Fall Vegtables.csv')
+fall = pd.read_csv('file_path\\Fall Vegtables.csv')
 fall.replace('Null', '0', inplace=True, regex= True)
 fall.rename(columns={'Catagory' : 'Category'}, inplace= True)
 
 to_float(fall, columns)
 fall['Preserve Jar Artisan'] = fall['Preserve Jar Artisan'].astype(float)
 #print(fall.dtypes)
-fall.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Fall Vegetables.parquet',engine='fastparquet')
+fall.to_parquet('file_path\\Fall Vegetables.parquet',engine='fastparquet')
 
-winter = pd.read_csv('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Winter Vegtables.csv')
+winter = pd.read_csv('file_path\\Project CSVs\\Winter Vegtables.csv')
 winter.replace('Null', '0', inplace= True, regex= True)
 winter.rename(columns={'Catagory': 'Category'}, inplace= True)
 
 to_float(winter, columns)
 winter['Preserve Jar Artisan'] = winter['Preserve Jar Artisan'].astype(float)
 #print(winter.dtypes)
-winter.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Winter Vegetables.parquet', engine='fastparquet')
+winter.to_parquet('file_path\\Winter Vegetables.parquet', engine='fastparquet')
 
 merged_veg = pd.concat([spring, summer, fall, winter])
-merged_veg.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Merged Vegetables.parquet', engine='fastparquet')
+merged_veg.to_parquet('file_path\\Merged Vegetables.parquet', engine='fastparquet')
 
-fish = pd.read_csv('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Fish Only.csv')
+fish = pd.read_csv('file_path\\Fish Only.csv')
 
 columns1 = [x for x in fish.columns if x != 'Name']
 to_float(fish, columns1)
@@ -79,6 +79,6 @@ display(fish)
 display(fish1)
 display(fish2)
 
-fish.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Fish Only.parquet', engine='fastparquet')
-fish1.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Fish Smoker.parquet', engine='fastparquet')
-fish2.to_parquet('C:\\Users\\17197\\Documents\\Coding Projects\\Project CSVs\\Fish Artisan Smoker.parquet', engine='fastparquet')
+fish.to_parquet('file_path\\Fish Only.parquet', engine='fastparquet')
+fish1.to_parquet('file_path\\Fish Smoker.parquet', engine='fastparquet')
+fish2.to_parquet('file_path\\Fish Artisan Smoker.parquet', engine='fastparquet')
